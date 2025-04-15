65272743ab880e4ae6ef1aaa8a9d1037863efe5c
Jonathan Locke <jon@thanlocke.com>
2025-04-15 15:23:03 -0600

    work on converters

--- src/main/kotlin/jonathanlocke/katalyst/nucleus/mixins/MixinReference.kt
+++ src/main/kotlin/jonathanlocke/katalyst/nucleus/mixins/MixinReference.kt
@@ -7,12 +7,8 @@ internal class MixinReference(val type: KClass<*>, val owner: Any) {
 
     override fun toString(): String = "${System.identityHashCode(owner)}:${type.simpleName}"
 
-    override fun equals(other: Any?): Boolean {
-        if (other is MixinReference) {
-            return this.type == other.type && this.owner === other.owner
-        }
-        return false
-    }
+    override fun equals(other: Any?): Boolean =
+        (other is MixinReference && this.type == other.type && this.owner === other.owner)
 
     override fun hashCode(): Int = Objects.hash(type, owner)
 }
